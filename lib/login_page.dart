201cc9773838e0ec2d671b2127770078c57a7a6c
FlutLab.io <>
2022-08-17 11:22:26 +0000

    Autosave

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,6 +9,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var SvgPicture;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
