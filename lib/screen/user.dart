b2d86cb77d57c14031cbe4c31d997472fe0c1718
FlutLab.io <>
2022-08-21 13:24:22 +0000

    Autosave

--- lib/screen/user.dart
+++ lib/screen/user.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
 
 class UserScreen extends StatelessWidget {
   const UserScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("User Screen")),
+    return const Scaffold(
+      body: Center(
+        child: ListTile(
+          title: Text(
+            'Address',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          leading: Icon(IconlyBold.location),
+          trailing: Icon(IconlyBold.arrowRight2),
+        ),
+      ),
     );
   }
 }
