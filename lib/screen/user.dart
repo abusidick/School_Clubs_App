570a1d659b00b1bb1c97a4b7a6f70ec3d779823c
FlutLab.io <>
2022-08-21 16:54:08 +0000

    Autosave

--- lib/screen/user.dart
+++ lib/screen/user.dart
@@ -9,40 +9,43 @@ class UserScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              RichText(
-                  text: const TextSpan(
-                      text: 'Hi, ',
-                      style: TextStyle(
-                          color: Colors.cyan,
-                          fontSize: 27,
-                          fontWeight: FontWeight.bold))),
-              const Divider(
-                thickness: 2,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              _listTiles(
-                  title: 'Address',
-                  icon: IconlyBold.location,
-                  onPressed: () {}),
-              _listTiles(
-                  title: 'Clubs', icon: IconlyBold.user3, onPressed: () {}),
-              _listTiles(
-                  title: 'Notifications',
-                  icon: IconlyBold.notification,
-                  onPressed: () {}),
-              _listTiles(
-                  title: 'Forget Password',
-                  icon: IconlyBold.lock,
-                  onPressed: () {}),
-              _listTiles(
-                  title: 'Log Out', icon: IconlyBold.logout, onPressed: () {}),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                    text: const TextSpan(
+                        text: 'Hi, ',
+                        style: TextStyle(
+                            color: Colors.cyan,
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold))),
+                const Divider(
+                  thickness: 2,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                _listTiles(
+                    title: 'Address',
+                    icon: IconlyBold.location,
+                    onPressed: () {}),
+                _listTiles(
+                    title: 'Clubs', icon: IconlyBold.user3, onPressed: () {}),
+                _listTiles(
+                    title: 'Notifications',
+                    icon: IconlyBold.notification,
+                    onPressed: () {}),
+                _listTiles(
+                    title: 'Forget Password',
+                    icon: IconlyBold.lock,
+                    onPressed: () {}),
+                _listTiles(
+                    title: 'Log Out', icon: IconlyBold.logout, onPressed: () {}),
+              ],
+            ),
           ),
         ),
       ),
