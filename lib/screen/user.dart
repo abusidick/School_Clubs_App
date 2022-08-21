cc5435df540cffd1c9bde2fa6bb4d6674838f226
FlutLab.io <>
2022-08-21 17:16:47 +0000

    Autosave

--- lib/screen/user.dart
+++ lib/screen/user.dart
@@ -34,9 +34,13 @@ class UserScreen extends StatelessWidget {
                       ]),
                 ),
                 TextWidget(
-                    text: 'email@gmail.com', color: Colors.black12, textSize: 20),
+                    text: 'email@gmail.com',
+                    color: Color(0x1f1b1919),
+                    textSize: 20),
                 TextWidget(
-                    text: 'Bsc Computer Science', color: Colors.black12, textSize: 20),
+                    text: 'Bsc Computer Science',
+                    color: Color(0x1f1b1919),
+                    textSize: 20),
                 const Divider(
                   thickness: 2,
                 ),
