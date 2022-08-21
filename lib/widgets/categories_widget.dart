e0b8b007cd6db23e13d076f6a86b79137cc16c38
FlutLab.io <>
2022-08-21 21:28:36 +0000

    Autosave

---
+++ lib/widgets/categories_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CategoriesWidget extends StatelessWidget {
+  const CategoriesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.red.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: Colors.red.withOpacity(0.7),
+            width: 2,
+          )),
+      child: Column(
+        children: [
+          Container(
+            height: _screenWidth * 0.3,
+            width: _screenWidth * 0.3,
+          ),
+        ],
+      ),
+    );
+  }
+}
