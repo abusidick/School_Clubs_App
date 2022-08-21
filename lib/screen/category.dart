6b3b455c2c18a8f3abaeb020845ea97ffe7a8f1a
FlutLab.io <>
2022-08-21 23:23:12 +0000

    Autosave

--- lib/screen/category.dart
+++ lib/screen/category.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/widgets/categories_widget.dart';
-import 'package:helloworld/widgets/text_widget.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Scaffold(
-        appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
           elevation: 0,
           title: TextWidget(
-              text: "Categories", color: Colors.cyan, textSize: 24,isTitle: true,),
-        ),
-        body: GridView.count(
+            text: "Categories",
+            color: Colors.cyan,
+            textSize: 24,
+            isTitle: true,
+          ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
           crossAxisCount: 2,
           childAspectRatio: 240 / 250,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
-          children: List.generate(8, (index) {
+          children: List.generate(6, (index) {
             return CategoriesWidget();
           }),
         ),
