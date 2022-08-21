161224c31988b7a3fdb810866092f40518cef139
FlutLab.io <>
2022-08-21 23:07:12 +0000

    Autosave

--- lib/widgets/categories_widget.dart
+++ lib/widgets/categories_widget.dart
@@ -17,26 +17,29 @@ class CategoriesWidget extends StatelessWidget {
               color: Colors.red.withOpacity(0.7),
               width: 2,
             )),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              height: _screenWidth * 0.35,
-              width: _screenWidth * 0.4,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(
-                        'images/People.jpg',
-                      ),
-                      fit: BoxFit.fill)),
-            ),
-            TextWidget(
-              text: 'Educational',
-              color: Colors.black,
-              textSize: 20,
-              isTitle: true,
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: _screenWidth * 0.35,
+                width: _screenWidth * 0.4,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                          'images/People.jpg',
+                        ),
+                        fit: BoxFit.fill)),
+              ),
+              TextWidget(
+                text: 'Educational',
+                color: Colors.black,
+                textSize: 20,
+                isTitle: true,
+              )
+            ],
+          ),
         ),
       ),
     );
