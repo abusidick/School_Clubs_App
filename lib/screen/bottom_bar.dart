9be97bd6fbf22db3ca950b8018024d87edfd226d
FlutLab.io <>
2022-08-21 13:52:42 +0000

    Autosave

--- lib/screen/bottom_bar.dart
+++ lib/screen/bottom_bar.dart
@@ -13,7 +13,7 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 3;
 
   final List _pages = [
     const HomeScreen(),
