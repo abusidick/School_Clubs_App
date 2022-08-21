71b774baa6bb127aa05716bc0cee2da5c658c5af
FlutLab.io <>
2022-08-21 10:39:35 +0000

    Autosave

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget  
 }
